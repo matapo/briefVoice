ca9bd224aa81dd1d4694ea1d75cf8c2287d5b4de
mata <matas.pocevicius@gmail.com>
2019-03-08 23:36:39 +0100

    update

--- hello/views.py
+++ hello/views.py
@@ -6,7 +6,7 @@ from .models import Greeting
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
-    Greetings.objects.create()
+    Greeting.objects.create()
 
     return render(request, "index.html")
 
