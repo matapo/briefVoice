9cc8770681411ceb529d6c43287ce1d18be28e43
Martynas <martynas.subonis@gmail.com>
2019-03-09 00:52:55 +0100

    Add signal models

--- hello/models.py
+++ hello/models.py
@@ -1,5 +1,12 @@
 from django.db import models
+from signal_names import signal_names_list
 
-# Create your models here.
+
 class Greeting(models.Model):
-    when = models.DateTimeField("date created", auto_now_add=True)
+    when = models.DateTimeField( date created , auto_now_add=True)
+
+class SignalData(models.Model):
+    time = models.DateTimeField( Time , auto_now_add=True)
+
+    for name in signal_names_list:
+        SignalData.add_to_class(name, models.Decimal()))
