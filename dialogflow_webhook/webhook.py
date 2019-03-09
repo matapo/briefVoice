33f392df302b85454e8154c153dda3fb5cade6fe
ressay <bourahla.yasser@gmail.com>
2019-03-09 11:08:41 +0100

    webhook trial

--- dialogflow_webhook/webhook.py
+++ dialogflow_webhook/webhook.py
@@ -2,26 +2,6 @@ from django.http import JsonResponse, HttpResponse
 
 
 def serve(request):
-    response = {
-  "payload": {
-    "google": {
-      "expectUserResponse": True,
-      "richResponse": {
-        "items": [
-          {
-            "simpleResponse": {
-              "textToSpeech": "I can send you alerts. Would you like that?"
-            }
-          }
-        ],
-        "suggestions": [
-          {
-            "title": "Alert me of new tips"
-          }
-        ]
-      }
-    }
-  }
-}
 
-    return JsonResponse(response,safe=False)
+
+    return JsonResponse({'fulfillmentText': 'my answer!'},safe=False)
