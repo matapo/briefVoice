cfa4a2f9ce734b91e01b4b3bfd54938b08638b92
ressay <bourahla.yasser@gmail.com>
2019-03-09 11:26:21 +0100

    webhook trial

--- dialogflow_webhook/webhook.py
+++ dialogflow_webhook/webhook.py
@@ -2,6 +2,40 @@ from django.http import JsonResponse, HttpResponse
 
 
 def serve(request):
-
-
-    return JsonResponse({'fulfillmentText': 'my answer!'},safe=False)
+    response = {
+  "conversationToken": "[]",
+  "expectUserResponse": True,
+  "expectedInputs": [
+    {
+      "inputPrompt": {
+        "richInitialPrompt": {
+          "items": [
+            {
+              "simpleResponse": {
+                "textToSpeech": "Hey! this is BriefVoice2"
+              }
+            }
+          ]
+        }
+      },
+      "possibleIntents": [
+        {
+          "intent": "assistant.intent.action.TEXT"
+        }
+      ],
+      "speechBiasingHints": [
+        "$try_entity"
+      ]
+    }
+  ],
+  "responseMetadata": {
+    "status": {
+      "message": "Success (200)"
+    },
+    "queryMatchInfo": {
+      "queryMatched": True,
+      "intent": "360ea1a4-a467-4508-ad52-29196aef4ea8"
+    }
+  }
+}
+    return JsonResponse(response,safe=False)
